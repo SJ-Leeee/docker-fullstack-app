96a9f352510793a9814cbaebf6f8e78118a2dcf9
leeseungjun <cjstkrak@gmail.com>
2024-04-18 19:37:21 +0900

    fix: githubActions

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,15 @@
 const mysql = require("mysql");
 
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
-});
-
+try {
+  const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
+  });
+} catch (err) {
+  console.log(err, process.env.MYSQL_PORT);
+}
 exports.pool = pool;

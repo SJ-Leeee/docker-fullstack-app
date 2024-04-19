85fd6949ee0c4e4d2a6a40f36659f38c2509a013
leeseungjun <cjstkrak@gmail.com>
2024-04-19 16:33:05 +0900

    fix: backend exports pool

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,11 @@
 const mysql = require("mysql");
 
-try {
-  const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT,
-  });
-} catch (err) {
-  console.log(err, process.env.MYSQL_PORT);
-}
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+});
 exports.pool = pool;
